123e007715b12f9b1e2904fd8ad1eb2724f210ab
Veronika Černá <69005656+werdylp@users.noreply.github.com>
2022-05-22 19:31:44 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace D5
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("zadej číslo, od kterého má řada začínat: ");
+            int zacatek = int.Parse(Console.ReadLine());
+            
+            Console.Write("zadej číslo, kterým má řada končit: ");
+            int konec = int.Parse(Console.ReadLine());
+
+            for (int i = zacatek; i <= konec; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
